bf2ce3c30ef5e8be27b9499925331ef5c3349601
Alejo <alejocarranza2017@gmail.com>
2023-01-28 00:27:58 -0300

    28/1 --- Varios errores al querer agregar el hovered de casas hizo que empecemos de nuevo el navbar (falta terminar)

--- static/js/base/script.js
+++ static/js/base/script.js
@@ -1,32 +1,60 @@
-((d, w)=>{
-    const navbar= function(){
-        if(w.innerWidth<= "660"){
-            const $btnContacto= d.getElementById("btn-contacto"),
-                $nav= d.getElementById("nav"),
-                $btnMenu= d.getElementById("hamburguesa");
+// ((d, w)=>{
+//     // Funcion para el menu hamburguesa
+//     const $btnContacto= d.getElementById("btn-contacto"),
+//         $nav= d.getElementById("nav"),
+//         $btnMenu= d.getElementById("hamburguesa");
 
-            $btnMenu.style.setProperty("cursor", "pointer");
+//     $btnMenu.style.setProperty("cursor", "pointer");
+//     $btnMenu.addEventListener('click', e=>{
+//         if($btnMenu.getAttribute("data-touched")=="false"){
+//             $nav.style.setProperty("left", "0");
+//             $nav.style.setProperty("right", "0");
+//             $btnContacto.style.setProperty("left", "0");
+//             $btnContacto.style.setProperty("right", "0");
 
-            $btnMenu.addEventListener('click', e=>{
-                if($btnMenu.getAttribute("data-touched")=="false"){
-                    $nav.style.setProperty("left", "0");
-                    $nav.style.setProperty("right", "0");
-                    $btnContacto.style.setProperty("left", "0");
-                    $btnContacto.style.setProperty("right", "0");
+//             $btnMenu.setAttribute("data-touched", "true");
+//         } else{
+//             $nav.style.setProperty("left", "auto");
+//             $nav.style.setProperty("right", "100vw");
+//             $btnContacto.style.setProperty("left", "auto");
+//             $btnContacto.style.setProperty("right", "100vw");
 
-                    $btnMenu.setAttribute("data-touched", "true");
-                } else{
-                    $nav.style.setProperty("left", "auto");
-                    $nav.style.setProperty("right", "100vw");
-                    $btnContacto.style.setProperty("left", "auto");
-                    $btnContacto.style.setProperty("right", "100vw");
+//             $btnMenu.setAttribute("data-touched", "false");
+//         };
+//     });
+// })(document, window);
 
-                    $btnMenu.setAttribute("data-touched", "false");
-                };
-            });
-        };
+// ((d,w)=>{
+//     // Funcion para hover en "casas"
+//     const $casas= d.getElementById("nav-casas"),
+//         $ul= d.getElementById("nav-ul-casas");
+
+//     $casas.addEventListener("mouseover", e=>{
+//         $ul.style.setProperty("display", "flex");
+//     });
+//     $casas.addEventListener("mouseout", e=>{
+//         $ul.style.setProperty("display", "none");
+//     });
+//     $ul.addEventListener("mouseover", e=>{
+//         $ul.style.setProperty("display", "flex");
+//     });
+//     $ul.addEventListener("mouseout", e=>{
+//         $ul.style.setProperty("display", "none");
+//     });
+
+// })(document, window);
+window.addEventListener("DOMContentLoaded", ()=>{
+
+
+    const d= document,
+        $submenu= d.getElementsByClassName("submenu__item");
+
+    for(let i=0; i < $submenu.length; i++){
+        $submenu[i].addEventListener("click", e=>{
+            location.href= $submenu[i].firstElementChild.getAttribute("href");
+        });
     };
-    w.addEventListener('resize', navbar);
-    w.addEventListener('load', navbar);
 
-})(document, window);
+
+});
+
