d1f1dc3a199f82a8e88c5c49f0e56262a299d210
Alejo <alejocarranza2017@gmail.com>
2023-01-29 00:34:10 -0300

    28/1 (error en el anterior) ---  Hicimos el navbar responsive (nos falta el submenu) y agregue fontawesome de una manera mejor

--- static/js/base/script.js
+++ static/js/base/script.js
@@ -1,60 +1,52 @@
-// ((d, w)=>{
-//     // Funcion para el menu hamburguesa
-//     const $btnContacto= d.getElementById("btn-contacto"),
-//         $nav= d.getElementById("nav"),
-//         $btnMenu= d.getElementById("hamburguesa");
-
-//     $btnMenu.style.setProperty("cursor", "pointer");
-//     $btnMenu.addEventListener('click', e=>{
-//         if($btnMenu.getAttribute("data-touched")=="false"){
-//             $nav.style.setProperty("left", "0");
-//             $nav.style.setProperty("right", "0");
-//             $btnContacto.style.setProperty("left", "0");
-//             $btnContacto.style.setProperty("right", "0");
-
-//             $btnMenu.setAttribute("data-touched", "true");
-//         } else{
-//             $nav.style.setProperty("left", "auto");
-//             $nav.style.setProperty("right", "100vw");
-//             $btnContacto.style.setProperty("left", "auto");
-//             $btnContacto.style.setProperty("right", "100vw");
-
-//             $btnMenu.setAttribute("data-touched", "false");
-//         };
-//     });
-// })(document, window);
-
-// ((d,w)=>{
-//     // Funcion para hover en "casas"
-//     const $casas= d.getElementById("nav-casas"),
-//         $ul= d.getElementById("nav-ul-casas");
-
-//     $casas.addEventListener("mouseover", e=>{
-//         $ul.style.setProperty("display", "flex");
-//     });
-//     $casas.addEventListener("mouseout", e=>{
-//         $ul.style.setProperty("display", "none");
-//     });
-//     $ul.addEventListener("mouseover", e=>{
-//         $ul.style.setProperty("display", "flex");
-//     });
-//     $ul.addEventListener("mouseout", e=>{
-//         $ul.style.setProperty("display", "none");
-//     });
-
-// })(document, window);
 window.addEventListener("DOMContentLoaded", ()=>{
 
-
-    const d= document,
+    ((d)=>{
+        // Autimatizo que al tocar el li se realiza la accion del a interior (submenu en +800px)
         $submenu= d.getElementsByClassName("submenu__item");
 
-    for(let i=0; i < $submenu.length; i++){
-        $submenu[i].addEventListener("click", e=>{
-            location.href= $submenu[i].firstElementChild.getAttribute("href");
-        });
-    };
+        for(let i=0; i < $submenu.length; i++){
+            $submenu[i].addEventListener("click", e=>{
+                location.href= $submenu[i].firstElementChild.getAttribute("href");
+            });
+        };
+    })(document);
+        
+    ((d, w)=>{
+        // Hago el funcionamiento del navbar responsive
+        const funcionalidadBtnMenu= function(){
+            const $menu= d.getElementsByClassName("menu")[0],
+                $contacto= d.getElementsByClassName("cta")[0];
 
+            $menu.classList.toggle("show");
+            $contacto.classList.toggle("show");
+        };
+        const btnMenu= function(){
+            if (w.innerWidth<= 800){
+                cont++;
+                if(cont==1) {
+                    const $boton= d.getElementById("btn-menu");
+                    $boton.addEventListener("click", funcionalidadBtnMenu);
+                }
+            }else{
+                cont=0;
+                const $boton= d.getElementById("btn-menu");
+                $boton.removeEventListener("click", funcionalidadBtnMenu); 
+            };
+        };
 
+        let cont= 0;
+        addEventListener("resize", btnMenu);
+        addEventListener("load", btnMenu);
+
+        // Hago el funcionamiento del submenu del navbar responsive
+        const btnSubmenu= function(){
+            const $botonSubmenu= d.getElementsByClassName("submenu-btn")[0];
+            // Cuidado! la variable es solo el a no el li
+            console.log($botonSubmenu)
+        };
+        btnSubmenu();
+
+    })(document, window);
+    
 });
 
