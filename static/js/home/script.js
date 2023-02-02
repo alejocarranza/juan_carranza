9aa80a8bf7627dab6aba6409ff2e41e2452e6af7
Alejo <alejocarranza2017@gmail.com>
2023-02-01 23:28:17 -0300

    1/2 --- Hice una parte responsive del navbar (me falta que queden las flechas siempre al medio y termino que sea responsive)

--- static/js/home/script.js
+++ static/js/home/script.js
@@ -1,42 +1,47 @@
 addEventListener("DOMContentLoaded", e=>{
-    const d= document;
+    
+    (d=>{ // SLIDER
+        // Hago las flechas del slider
+        const $last= d.getElementById("s-last"),
+            $first= d.getElementById("s-first");
 
-    // Hago el slider
-    const $last= d.getElementById("s-last"),
-        $first= d.getElementById("s-first");
+        const slide= function($last, $first, side){
+            const $actual= d.getElementsByClassName("s-active")[0];
+                
+            $actual.classList.toggle("s-active");
 
-    const slide= function($last, $first, side){
-        const $actual= d.getElementsByClassName("s-active")[0];
-            
-        $actual.classList.toggle("s-active");
-
-        if(side=="forward"){
-            if ($actual === $last) {
-                $first.classList.toggle("s-active");
-            } else {
-                $actual.nextElementSibling.classList.toggle("s-active");
-            };
-        }else{
-            if ($actual === $first) {
-                $last.classList.toggle("s-active");
-            } else {
-                $actual.previousElementSibling.classList.toggle("s-active");
+            if(side=="forward"){
+                if ($actual === $last) {
+                    $first.classList.toggle("s-active");
+                } else {
+                    $actual.nextElementSibling.classList.toggle("s-active");
+                };
+            }else{
+                if ($actual === $first) {
+                    $last.classList.toggle("s-active");
+                } else {
+                    $actual.previousElementSibling.classList.toggle("s-active");
+                };
             };
         };
-    };
-    
-    setInterval(() => {
-        slide($last, $first, "forward");
-    }, 6000);
-    
-    setTimeout(() => {
-        const $aLeft= d.getElementById("a-left"),
-            $aRight= d.getElementById("a-right");
-        $aLeft.addEventListener("click", ()=>{
-            slide($last, $first, "back");
-        });
-        $aRight.addEventListener("click", ()=>{
+
+        setInterval(() => {
             slide($last, $first, "forward");
-        });
-    }, 0);
+        }, 6000);
+
+        setTimeout(() => {
+            const $aLeft= d.getElementById("a-left"),
+                $aRight= d.getElementById("a-right");
+            $aLeft.addEventListener("click", ()=>{
+                slide($last, $first, "back");
+            });
+            $aRight.addEventListener("click", ()=>{
+                slide($last, $first, "forward");
+            });
+        }, 0);
+
+    })(document);
+    
+
+    // Hago el slider responsive
 });
