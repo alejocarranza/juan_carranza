ea05fc2d496309f3f060b67f882c3fc93ed66ed8
Alejo <alejocarranza2017@gmail.com>
2023-02-09 00:04:38 -0300

    8/2 --- Continue con el backend de casas

--- casas/admin.py
+++ casas/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import Casa
 from .models import Barrio
+from .models import Imagen
 
 # Register your models here.
 
-admin.site.register(Casa)
+class CasaInline(admin.TabularInline):
+    model= Casa
+    extra= 0
+
+class BarrioAdmin(admin.ModelAdmin):
+    # Read
+    search_fields= ("nombre")
+    list_filter= ("privado")
+
+class CasaAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Barrio)
+admin.site.register(Imagen)
+admin.site.register(Casa)
--- casas/models.py
+++ casas/models.py
@@ -6,15 +6,39 @@ class Barrio(models.Model):
     nombre= models.CharField(max_length= 80)
     direccion= models.CharField(max_length=200)
     privado= models.BooleanField(default= "True")
+    slug= models.SlugField(max_length=150)
+    created= models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name= "barrio"
+        verbose_name_plural= "barrios"
+
+    def __str__(self):
+        return self.nombre
+
+
+class Imagen(models.Model):
+    imagen= models.ImageField(upload_to= "casas/imagenes/")
+
+    class Meta:
+        verbose_name= "imagen"
+        verbose_name_plural= "imagenes"
 
 class Casa(models.Model):
     direccion= models.ForeignKey(Barrio, on_delete=models.CASCADE)
+    slug= models.SlugField(max_length=150)
     lote= models.IntegerField(null=True, blank=True)
     manzana= models.IntegerField(null=True, blank=True)
+    imagenes= models.ManyToManyField(Imagen)
 
+    descripcion_corta= models.TextField(max_length=1000)
+    descripcion_larga= models.TextField(max_length=4000)
 
-    cantidadImagenes= models.IntegerField(default= 1)
-    imagenes= []
-    for i in self.cantidadImagenes():
-        imagenes[i]= models.ImageField()
+    created= models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name= "casa"
+        verbose_name_plural= "casas"
+
+    def __str__(self):
+        return str(self.slug)
