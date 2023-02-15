978fe5ab3dcf2bdb1c9c40a8e8c28daa60bea31d
Alejo <alejocarranza2017@gmail.com>
2023-02-14 22:29:52 -0300

    Termine la cuarta parte de casaX (front y back)

--- static/js/casas/casaX/script.js
+++ static/js/casas/casaX/script.js
@@ -1,13 +1,15 @@
+const d= document;
+
 function showImages(option) {
-  document.querySelectorAll('.house-images').forEach(elem => {
+  d.querySelectorAll('.house-images').forEach(elem => {
     elem.style.display = 'none';
   });
-  document.querySelector(`#${option}`).style.display = 'flex';
+  d.querySelector(`#${option}`).style.display = 'flex';
 };
 
-document.querySelectorAll('#house-options-images h3').forEach(elem => {
+d.querySelectorAll('#house-options-images h3').forEach(elem => {
   elem.addEventListener('click', () => {
-    document.querySelectorAll('#house-options-images h3').forEach(elem => {
+    d.querySelectorAll('#house-options-images h3').forEach(elem => {
       elem.classList.remove('house-options-active');
     });
     elem.classList.add('house-options-active');
@@ -17,16 +19,16 @@ document.querySelectorAll('#house-options-images h3').forEach(elem => {
 
 showImages('planos');
 
-const houseOptions = document.querySelector("#house-options-images");
-const houseImages = document.querySelectorAll(".house-images");
+const houseOptions = d.querySelector("#house-options-images");
+const houseImages = d.querySelectorAll(".house-images");
 
 houseOptions.addEventListener("click", function(event) {
   if (event.target.tagName === "H3") {
-    const activeImage = document.querySelector(".house-images.active");
+    const activeImage = d.querySelector(".house-images.active");
     if (activeImage) {
       activeImage.classList.remove("active");
     }
-    const targetImage = document.querySelector(`#${event.target.innerText.toLowerCase()}`);
+    const targetImage = d.querySelector(`#${event.target.innerText.toLowerCase()}`);
     targetImage.classList.add("active");
   }
 });
@@ -34,11 +36,11 @@ houseOptions.addEventListener("click", function(event) {
 
 // slider
 
-const $last= document.getElementById("sa-last"),
-  $first= document.getElementById("sa-first");
+const $last= d.getElementById("sa-last"),
+  $first= d.getElementById("sa-first");
 
 const slide= function($last, $first, side){
-  const $actual= document.getElementsByClassName("sa-active")[0];
+  const $actual= d.getElementsByClassName("sa-active")[0];
       
   $actual.classList.toggle("sa-active");
 
@@ -57,74 +59,26 @@ const slide= function($last, $first, side){
   };
 };
 
-// document.addEventListener("DOMContentLoaded", e=>{
-//   setTimeout(()=>{
-//     let intervalActive= false;
-  
-//     const observer = new IntersectionObserver(entries=> {
-//       // if (entry.isIntersecting && !(intervalActive)){
-//       //   
-//       // } else if (!(entry.isIntersecting) && interval){
-//       //   interval.remove();
-//       // };
-//       entries.forEach(entry=>{
-//         if (entry.isIntersecting){
-//           let interval= setInterval(() => {
-//             slide($last, $first, "forward");
-//           }, 4000);
-//           intervalActive= true;
-//         };
-        
-//         console.log(intervalActive);
-//         if(intervalActive){
-//           clearInterval(interval);
-//           intervalActive= false;
-//         };
-//       });
-//     });
-  
-//     console.log(document.getElementById("avances-slider"))
-//     observer.observe(document.getElementById("avances-slider"))
-//   }, 0);  
-// });
+function startInterval(callback, interval) {
+  return setInterval(callback, interval);
+};
 
-// Seleccionamos el elemento que queremos observar
-// const element = document.getElementById("avances-slider");
+function stopInterval(intervalId) {
+  clearInterval(intervalId);
+};
 
-// // Definimos el observer
-// const observer = new IntersectionObserver(function (entries) {
-//   // Entries es un arreglo con los elementos observados
-//   entries.forEach(function (entry) {
-//     // Si el elemento está en pantalla
-//     if (entry.isIntersecting) {
-//       // Agregamos el setInterval
-//       const intervalId = setInterval(function () {
-//         // Aquí escribimos el código que queremos que se ejecute cada intervalo
-//       }, 1000);
+let slideInterval;
+const callback = function (entry){
+  console.log(entry[0].isIntersecting)
+  if(entry[0].isIntersecting){
+    slideInterval = startInterval(function () {
+      slide($last, $first, "forward");
+    }, 3500);    
+  } else{
+    stopInterval(slideInterval);
+  };
+};
 
-//       // Guardamos el id del interval para poder eliminarlo más tarde
-//       entry.target.intervalId = intervalId;
+const observer= new IntersectionObserver(callback, {threshold: 0.1,});
+observer.observe(d.getElementById("avances-slider"));
 
-//       // Detenemos el observer para este elemento
-//       observer.unobserve(entry.target);
-//     }
-//   });
-// });
-
-// // Iniciamos el observer para el elemento seleccionado
-// observer.observe(element);
-
-// // Para eliminar el setInterval cuando el elemento ya no está en pantalla, podemos agregar un event listener para el evento "slide"
-// element.addEventListener("slide", function () {
-//   // Si el elemento ya no está en pantalla
-//   if (!element.intervalId) {
-//     return;
-//   }
-
-//   // Eliminamos el setInterval
-//   clearInterval(element.intervalId);
-//   element.intervalId = null;
-
-//   // Re-iniciamos el observer para el elemento
-//   observer.observe(element);
-// });
